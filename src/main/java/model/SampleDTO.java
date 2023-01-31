58e2d1ea55410c5bb79545bb80e923a64cfc1376
DESKTOP-J97E72B\BIT <>
2023-01-31 15:45:46 +0900

    230131 까지 내용

---
+++ src/main/java/model/SampleDTO.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+    private int id;
+    private int prop1;
+    private String title;
+    private String something;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
